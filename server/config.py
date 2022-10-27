208f82026525e425500873dc6fd4b841eb8ff65c
Aleksei Rutkovskii <arutkovskii@umass.edu>
2022-10-27 11:03:22 -0400

    minor additions

---
+++ server/config.py
@@ -0,0 +1,28 @@
+import os
+# from dotenv import load_dotenv
+
+class Config:
+    # Locations
+    SERVER_DIR = SCRIPTS_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+
+    # Keys and Secrets
+    # FLASK_SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+
+    with open(os.path.join(CONFIG_DIR, 'env.txt')) as e:
+        ENVIRONMENT = e.read().strip()
+
+    if ENVIRONMENT == 'local':
+        pass
+
+    elif ENVIRONMENT == 'server'
+        pass
+
+
+
+
+
+
+
+print(Config.SERVER_DIR)
