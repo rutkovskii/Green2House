1b9c86537af76716e0def9a585aa09ce19c5cc8f
rutkovskii <arutkovskii@umass.edu>
2022-12-26 23:01:48 -0500

    change to wifi.sh

--- bbb/wifi.sh
+++ bbb/wifi.sh
@@ -6,7 +6,7 @@
 #:wq!
 
 echo "starting wifi"
-wpa_passphrase "<Your WIFI Name>" "Your Password" | sudo tee -a /etc/wpa_supplicant.conf
+wpa_passphrase "<Your WIFI Name>" "<Your Password>" | sudo tee -a /etc/wpa_supplicant.conf
 sudo wpa_supplicant -B -c /etc/wpa_supplicant.conf -i wlan0
 sudo dhclient wlan0
 echo "started wifi"
