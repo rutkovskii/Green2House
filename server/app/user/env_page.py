081726358a46c937aeaab45e68c9a228905b60e7
rutkovskii <arutkovskii@umass.edu>
2023-02-26 02:19:36 -0500

    gathered information on cabbage, adjusted values of the sliders based on the recommendations for the temperature and humidty

--- server/app/user/env_page.py
+++ server/app/user/env_page.py
@@ -1,8 +1,9 @@
 from flask import request, Blueprint, render_template, session
+from flask_login import login_required
 from datetime import datetime as dt
 import requests
 import json
-from flask_login import login_required
+
 
 env_page_bp = Blueprint('env_page_bp', __name__)
 
@@ -14,18 +15,6 @@ url = f'{bbb}/instructions'
 @login_required
 def set_environment():
     if request.method == 'POST':
-        # temperature = request.form['temperature']
-        # humidity = request.form['humidity']
-        times_per_day = request.form['times_per_day']
-        timestamp = str(round(dt.timestamp(dt.now())))
-
-        min_temperature = request.form['temperature_min']
-        max_temperature = request.form['temperature_max']
-        min_humidity = request.form['humidity_min']
-        max_humidity = request.form['humidity_max']
-        print(min_temperature, max_temperature)
-        print(min_humidity, max_humidity)
-
         body = json.dumps(
             {
                 'user_id': session['user_id'],
@@ -33,8 +22,9 @@ def set_environment():
                 'max_temperature': request.form['temperature_max'],
                 'min_humidity': request.form['humidity_min'],
                 'max_humidity': request.form['humidity_max'],
-                'times_per_day': times_per_day,
-                'timestamp': timestamp
+                'daily_water_freq': request.form['daily_water_freq'],
+                'water_amount_per_freq': request.form['water_amount_per_freq'],
+                'timestamp': str(round(dt.timestamp(dt.now())))
             }
         )
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
@@ -42,11 +32,17 @@ def set_environment():
         print(f"Status Code: {r.status_code}, Response: {r.json()}")
 
     # generate options for the times per day dropdown
-    times_per_day_options = ''
+    daily_water_freq_options = ''
     for i in range(1, 49):
-        times_per_day_options += f'<option value="{i}">{i}</option>'
+        daily_water_freq_options += f'<option value="{i}">{i}</option>'
+
+    # generate options for the water amount dropdown
+    water_amount_per_freq_options = ''
+    for j in range(50, 501, 50):
+        water_amount_per_freq_options += f'<option value="{j}">{j}</option>'
 
     # render the template with the options for the dropdown
     return render_template('user_env_page.html',
-                           times_per_day_options=times_per_day_options,
+                           daily_water_freq_options=daily_water_freq_options,
+                           water_amount_per_freq_options=water_amount_per_freq_options,
                            title='Set ENV — Green2House')
