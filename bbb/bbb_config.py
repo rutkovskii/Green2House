317b86d0efbea85a168b2ee0d167deb2a64dec35
Aleksei Rutkovskii <70371908+rutkovskii@users.noreply.github.com>
2023-03-28 14:33:37 -0400

    Dev stephen (#18)
    * md file update
    
    * updated functions, NOT DEBUGGED YET
    
    * Not debugged as of 3/27
    
    ---------
    
    Co-authored-by: Stephen Morelli <smorelli@umass.edu>

--- bbb/bbb_config.py
+++ bbb/bbb_config.py
@@ -11,20 +11,30 @@ class BBB_CONFIG:
     # LOG_DIR = os.path.join(SCRIPTS_DIR,'logs')
 
     # Files
-    font_adafruit = os.path.join(FILES_DIR, 'font5x8.bin')
+    #font_adafruit = os.path.join(FILES_DIR, 'font5x8.bin')
+    #print(font_adafruit)
 
     # Pins
     pins_dict = {
-        'pump_relay_pin': "P8_8",
-        'temp_relay_pin': "P8_10",
-        'button_pin': "P8_14"
+        'h_bridge1': "P8_12",
+        'h_bridge2': "P8_14",
+        'pump_relay_pin': "P8_16",
+        'fan_relay_pin': "P8_18",
+        'heater_relay_pin': "P8_26",
+        'adc_pin': "AIN0",
     }
 
     # Variables
-    threshold = 75
+    threshold = 75 #desired temperature in degrees F
+    desiredHum = 50 #desired humidity in percentage RH
     toggle = 0
     user_id = 1
     variance = 1
+
+    #minADC = wet, maxADC = dry
+    minADC = 1450 #the lowest value I measured with the soil moisture sensor
+    maxADC = 3250 #the highest value I measured with the soil moisture sensor
+
 
     # change link based on ngrok
     # must have internet connection
---
+++ bbb/docs/pinout.md
@@ -0,0 +1,22 @@
+# BeagleBone Pinout
+
+P9_01: GND
+
+P9_03: 3.3V (optional)
+
+P9_05: 5V (relays require 5V)
+
+
+
+## I2C (OLED and DHT20)
+
+P9_19: SCL
+
+P9_20: SDA
+
+## Relays 
+P8_08: Pump Relay
+
+P8_10: Heater Relay
+
+P8_14: Water Pump Button to Relay
