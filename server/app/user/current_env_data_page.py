4227d84a1abd1317d20dd95cafe0ff0c67e5f884
Aleksei Rutkovskii <70371908+rutkovskii@users.noreply.github.com>
2023-05-04 21:18:09 -0400

    charts, try except for sending, favicon, soil percent, navbar admin edits (#27)

--- server/app/user/current_env_data_page.py
+++ server/app/user/current_env_data_page.py
@@ -5,29 +5,41 @@ from app.database.models import Instructions
 from app.database.database import session_scope
 from app.user.config import UserConfig
 
-current_env_data_page_bp = Blueprint('current_env_data_page_bp', __name__)
+current_env_data_page_bp = Blueprint("current_env_data_page_bp", __name__)
 
 
-@current_env_data_page_bp.route(UserConfig.CURRENT_ENV_DATA_ROUTE, methods=['GET'])
+@current_env_data_page_bp.route(UserConfig.CURRENT_ENV_DATA_ROUTE, methods=["GET"])
 @login_required
 def current_env_data_page():
     """Returns the current environment settings page."""
     with session_scope() as s:
-        instructions = s.query(Instructions).filter(
-            Instructions.user_id == session['user_id']).order_by(Instructions.id.desc()).first()
+        instructions = (
+            s.query(Instructions)
+            .filter(Instructions.user_id == session["user_id"])
+            .order_by(Instructions.id.desc())
+            .first()
+        )
 
         print(instructions)
 
         if not instructions:
-            return render_template('/user_current_env_data_page.html', title='Current Environment Data', instructions=None)
+            return render_template(
+                "/user_current_env_data_page.html",
+                title="Current Environment Data — G2H",
+                instructions=None,
+            )
 
         s.expunge(instructions)
 
-        date = instructions.timestamp.strftime('%Y-%m-%d')
-        time = instructions.timestamp.strftime('%H:%M:%S')
+        date = instructions.timestamp.strftime("%Y-%m-%d")
+        time = instructions.timestamp.strftime("%H:%M:%S")
 
         # Add date and time to the instructions object as new attributes
         instructions.date = date
         instructions.time = time
 
-    return render_template('/user_current_env_data_page.html', title='Current Environment Data', instructions=instructions)
+    return render_template(
+        "/user_current_env_data_page.html",
+        title="Current Environment Data — G2H",
+        instructions=instructions,
+    )
